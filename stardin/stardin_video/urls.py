829e48db161ca0117cfe52280680fbd673615268
rgmcwill <rossmcwilliams360@gmail.com>
2022-01-04 14:30:58 -0600

    Work on video player

--- stardin/stardin_video/urls.py
+++ stardin/stardin_video/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page, name='index'),
+    path('', views.main_page, name='main_page',),
+    path('login', views.login, name='login'),
+    path('videos', views.videos, name='videos'),
     path('video/<uuid:video_id>/', views.video, name='video'),
     path('player/<uuid:video_id>/', views.video_player, name='player'),
 ]
--- stardin/stardin_video/views.py
+++ stardin/stardin_video/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+import requests
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 from .models import Video, Episode_Data
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the stardin_video index.")
-
 def main_page(request):
+    return HttpResponse("<a href='login'>Login</a>")
+
+def login(request):
+    print(request.method)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('videos')
+
+        else:
+            return HttpResponse('User Not Found!')
+    elif request.method == 'GET':
+        return render(request, 'login.html')
+    return HttpResponse("Was not a GET or POST request")
+
+# @login_required
+def videos(request):
+    print(request.headers)
     videos = Video.objects.all
 
     context = {
@@ -14,8 +36,10 @@ def main_page(request):
     }
     response = render(request, 'main_page.html', context)
     response.set_cookie('QWERTY', 'Testing')
+    print(response.headers)
     return response
 
+# @login_required
 def video(request, video_id):
     video = Video.objects.get(uuid=video_id)
 
@@ -24,6 +48,7 @@ def video(request, video_id):
     }
     return render(request, 'video.html', context)
 
+# @login_required
 def video_player(request, video_id):
     video = Video.objects.get(uuid=video_id)
 
